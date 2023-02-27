5da37dbffb8a62842010ac8800c59883407dc8e9
SoV3n04ek <mr.ivan121212@gmail.com>
2023-02-27 23:34:31 +0200

    homework-1-part-9 added todos

--- homeworks/homework-1/homework-1-part-9/src/Main.java
+++ homeworks/homework-1/homework-1-part-9/src/Main.java
@@ -1,6 +1,19 @@
 
 public class Main
 {
+    public static void initArrayRandomNumbers()
+    {
+        // FIXME add array initializer
+    }
+
+    public static void findMaxValue()
+    {}
+
+    public static void findMinValue()
+    {}
+
+
+
     public static void main(String[] args)
     {
         int[] numbers = {19, 10, 8, 17, 9};
