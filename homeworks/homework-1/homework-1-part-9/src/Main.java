231595a1f636e6bbbb16cfe8297abfae2ab227e7
SoV3n04ek <mr.ivan121212@gmail.com>
2023-02-28 23:46:35 +0200

    homework-1-part-9 done

--- homeworks/homework-1/homework-1-part-9/src/Main.java
+++ homeworks/homework-1/homework-1-part-9/src/Main.java
@@ -9,17 +9,83 @@ public class Main
 
         for (int i = 0; i < countNumbers; i++)
         {
-            arr[i] = random.nextInt(100);
+            arr[i] = random.nextInt(100) - 50;
         }
     }
 
-    public static void findMaxValue()
-    {}
+    public static int findMaxValue(int[] arr, int countNumbers)
+    {
+        int max = arr[0];
+        for (int i = 1; i < countNumbers; i++)
+        {
+            if (max < arr[i])
+            {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
 
-    public static void findMinValue()
-    {}
+    public static int findMinValue(int[] arr, int countNumbers)
+    {
+        int min = arr[0];
+        for (int i = 1; i < countNumbers; i++)
+        {
+            if (min > arr[i])
+            {
+                min = arr[i];
+            }
+        }
+        return min;
+    }
 
+    public static int countNegativeNumbers(int[] arr, int countNumbers)
+    {
+        int count = 0;
+        for (int i = 0; i < countNumbers; i++)
+        {
+            if (0 > arr[i])
+            {
+                count++;
+            }
+        }
+        return count;
+    }
 
+    public static int countPositiveNumbers(int[] arr, int countNumbers)
+    {
+        int count = 0;
+        for (int i = 0; i < countNumbers; i++)
+        {
+            if (arr[i] > 0)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static int countZeroNumbers(int[] arr, int countNumbers)
+    {
+        int count = 0;
+        for (int i = 0; i < countNumbers; i++)
+        {
+            if (arr[i] == 0)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void printArray(int[] numbers, int countNumbers)
+    {
+        for (int i = 0; i < countNumbers; i++)
+        {
+            System.out.print(numbers[i] + " ");
+        }
+        System.out.print("\n");
+    }
 
     public static void main(String[] args)
     {
@@ -31,9 +97,12 @@ public class Main
 
         initArrayRandomNumbers(numbers, countNumbers);
 
-        for (int i = 0; i < countNumbers; i++)
-        {
-            System.out.print(numbers[i] + " ");
-        }
+        printArray(numbers, countNumbers);
+
+        System.out.println("\n\nMax: " + findMaxValue(numbers, countNumbers));
+        System.out.println("Min: " + findMinValue(numbers, countNumbers));
+        System.out.println("\nCount numbers > 0: " + countPositiveNumbers(numbers, countNumbers));
+        System.out.println("Count numbers < 0: " + countNegativeNumbers(numbers, countNumbers));
+        System.out.println("Count numbers == 0: " + countZeroNumbers(numbers, countNumbers));
     }
 }
