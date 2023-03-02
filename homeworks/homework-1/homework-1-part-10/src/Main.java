3db1fed8ec754a049addb28cafb5b3aacf09a6a7
SoV3n04ek <mr.ivan121212@gmail.com>
2023-03-02 22:54:10 +0200

    homework-1-part-10 functions done

--- homeworks/homework-1/homework-1-part-10/src/Main.java
+++ homeworks/homework-1/homework-1-part-10/src/Main.java
@@ -13,7 +13,117 @@ public class Main
         }
     }
 
-    
+    public static int[] getArrayWithOnlyEvenNumbers(int[] arr)
+    {
+        int countEvenNumbers = 0;
+        for (int i = 0; i < arr.length; i++)
+        {
+            if (arr[i] % 2 == 0)
+            {
+                countEvenNumbers++;
+            }
+        }
+
+        if (countEvenNumbers == 0)
+            return null;
+
+        int[] evenNumbersArr = new int [countEvenNumbers];
+
+        int j = 0;
+        for (int i = 0; i < arr.length; i++)
+        {
+            if (arr[i] % 2 == 0)
+            {
+                evenNumbersArr[j++] = arr[i];
+            }
+        }
+
+        return evenNumbersArr;
+    }
+
+    public static int[] getArrayWithOnlyUnevenNumbers(int[] arr)
+    {
+        int countUnevenNumbers = 0;
+        for (int i = 0; i < arr.length; i++)
+        {
+            if (arr[i] % 2 != 0)
+            {
+                countUnevenNumbers++;
+            }
+        }
+
+        if (countUnevenNumbers == 0)
+            return null;
+
+        int[] unevenNumbersArr = new int [countUnevenNumbers];
+
+        int j = 0;
+        for (int i = 0; i < arr.length; i++)
+        {
+            if (arr[i] % 2 != 0)
+            {
+                unevenNumbersArr[j++] = arr[i];
+            }
+        }
+
+        return unevenNumbersArr;
+    }
+
+    public static int[] getArrayWithOnlyNegativeNumbers(int[] arr)
+    {
+        int countNegativeNumbers = 0;
+        for (int i = 0; i < arr.length; i++)
+        {
+            if (arr[i] < 0)
+            {
+                countNegativeNumbers++;
+            }
+        }
+
+        if (countNegativeNumbers == 0)
+            return null;
+
+        int[] negativeNumbersArr = new int [countNegativeNumbers];
+
+        int j = 0;
+        for (int i = 0; i < arr.length; i++)
+        {
+            if (arr[i] < 0)
+            {
+                negativeNumbersArr[j++] = arr[i];
+            }
+        }
+
+        return negativeNumbersArr;
+    }
+
+    public static int[] getArrayWithOnlyPositiveNumbers(int[] arr)
+    {
+        int countPositiveNumbers = 0;
+        for (int i = 0; i < arr.length; i++)
+        {
+            if (arr[i] > 0)
+            {
+                countPositiveNumbers++;
+            }
+        }
+
+        if (countPositiveNumbers == 0)
+            return null;
+
+        int[] positiveNumbersArr = new int [countPositiveNumbers];
+
+        int j = 0;
+        for (int i = 0; i < arr.length; i++)
+        {
+            if (arr[i] > 0)
+            {
+                positiveNumbersArr[j++] = arr[i];
+            }
+        }
+
+        return positiveNumbersArr;
+    }
 
     public static void main(String[] args)
     {
@@ -23,6 +133,6 @@ public class Main
         int count = scanner.nextInt();
         int[] arr = new int [count];
 
-
+        
     }
 }
